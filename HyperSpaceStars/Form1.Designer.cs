b3b31ae0f2551953a73747714f2daffd48bf9c14
horiz <horiz@Horizon.com>
2024-11-02 13:41:23 -0400

    Add project files.

---
+++ HyperSpaceStars/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HyperSpaceStars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.skglCanvas = new SkiaSharp.Views.Desktop.SKGLControl();
+            this.timerCanvas = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skglCanvas
+            // 
+            this.skglCanvas.BackColor = System.Drawing.Color.Black;
+            this.skglCanvas.Location = new System.Drawing.Point(0, -1);
+            this.skglCanvas.Name = "skglCanvas";
+            this.skglCanvas.Size = new System.Drawing.Size(917, 603);
+            this.skglCanvas.TabIndex = 0;
+            this.skglCanvas.VSync = false;
+            this.skglCanvas.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs>(this.skglCanvas_PaintSurface);
+            // 
+            // timerCanvas
+            // 
+            this.timerCanvas.Interval = 20;
+            this.timerCanvas.Tick += new System.EventHandler(this.timerCanvas_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 600);
+            this.Controls.Add(this.skglCanvas);
+            this.Name = "Form1";
+            this.Text = "Hyperspace Stars Simulation";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private SkiaSharp.Views.Desktop.SKGLControl skglCanvas;
+        private System.Windows.Forms.Timer timerCanvas;
+    }
+}
+
---
+++ HyperSpaceStars/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using SkiaSharp;
+using System.Windows.Forms;
+
+namespace HyperSpaceStars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BuildStarsArray();
+            timerCanvas.Enabled = true;
+        }
+
+        struct Star
+        {
+            public int startX;
+            public int startY;
+            public double angle;
+            public double speed;
+        }
+
+        static int numOfStars = 100;
+        Random random = new Random();
+        SKPaint paint = new SKPaint {
+            Color = SKColors.FloralWhite,
+            StrokeWidth = 2,
+            Style = SKPaintStyle.Fill
+        };
+        Star[] stars = new Star[numOfStars];
+
+        private void BuildStarsArray()
+        {
+            for (int i = 0; i < numOfStars; i++)
+            {
+                stars[i].startX = random.Next(0, skglCanvas.Width);
+                stars[i].startY = random.Next(0, skglCanvas.Height);
+                stars[i].angle = Math.Atan2(stars[i].startY - skglCanvas.Height/2, stars[i].startX - skglCanvas.Width/2);
+                stars[i].speed = random.Next(5, 10);
+            }
+        }
+
+        private void skglCanvas_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            e.Surface.Canvas.Clear(SKColors.Black);
+            for (int i = 0; i < numOfStars; i++)
+            {
+                stars[i].startX += Convert.ToInt32(stars[i].speed * Math.Cos(stars[i].angle));
+                stars[i].startY += Convert.ToInt32(stars[i].speed * Math.Sin(stars[i].angle));
+                if (stars[i].startX < 0 || stars[i].startX > skglCanvas.Width || stars[i].startY < 0 || stars[i].startY > skglCanvas.Height)
+                {
+                    stars[i].startX = random.Next(0, skglCanvas.Width);
+                    stars[i].startY = random.Next(0, skglCanvas.Height);
+                    stars[i].angle = Math.Atan2(stars[i].startY - skglCanvas.Height / 2, stars[i].startX - skglCanvas.Width / 2);
+                    stars[i].speed = random.Next(5, 10);
+                }
+                SKPoint point = new SKPoint(stars[i].startX, stars[i].startY);
+                e.Surface.Canvas.DrawPoint(point, paint);
+
+                //Draw tail
+                int nextX = stars[i].startX;
+                int nextY = stars[i].startY;
+                int tailL = random.Next(2, 10);
+                for (int j = 0; j < tailL; j++)
+                {
+                    nextX += Convert.ToInt32(j * Math.Cos(stars[i].angle));
+                    nextY += Convert.ToInt32(j * Math.Sin(stars[i].angle));
+                    SKPoint tPoint = new SKPoint(nextX, nextY);
+                    e.Surface.Canvas.DrawPoint(tPoint, paint);
+                }
+            }
+        }
+
+        private void timerCanvas_Tick(object sender, EventArgs e)
+        {
+            skglCanvas.Invalidate();
+            Application.DoEvents();
+        }
+
+    }
+}
